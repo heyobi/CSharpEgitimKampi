ac6c1fcd2168857b91daa568cc7d89311e17a0e8
İbrahim Polat <62967337+heyobi@users.noreply.github.com>
2024-10-02 02:08:52 +0300

    Proje dosyası ekle.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region YazdırmaKotmuları
+            //Console.Write("Merhaba Dünya\n");
+            //Console.WriteLine("Selam");
+
+            Console.WriteLine("***** Yenej Katogorileri *****");
+            Console.WriteLine("");
+            Console.WriteLine("1 -> Çorba");
+            Console.WriteLine("1 -> Çorba");
+            Console.WriteLine("1 -> Çorba");
+            Console.WriteLine("1 -> Çorba");
+            Console.WriteLine("1 -> Çorba");
+            Console.WriteLine("1 -> Çorba");
+            Console.WriteLine("1 -> Çorba");
+
+            Console.WriteLine("***** Yenej Katogorileri *****");
+
+            #endregion
+
+            #region Değişkenler
+            string name = "Muratt";
+            name = "İbrahim";
+            Console.WriteLine(name);
+
+            #endregion
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+            Console.WriteLine(hamburgerPrice + cokePrice);
+
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            double x = 10.2;
+            double y = x + 2;
+           
+            Console.WriteLine(x + y + " \u20BA");
+            Console.ReadKey();
+        }
+    }
+}
